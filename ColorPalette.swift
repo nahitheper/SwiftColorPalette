51a2142444e33f562a9c8f8de1e6637dd2cd64d3
Nahit Rüştü HEPER <nahitheper@gmail.com>
2016-04-06 17:59:29 +0300

    added demo content

---
+++ ColorPalette.swift
@@ -0,0 +1,14 @@
+enum ColorPalette {
+
+    static let LightGrayColor =  UIColor(red:0.737, green: 0.769, blue: 0.792, alpha: 1.000)
+
+    static let BlueColor =  UIColor(red:0.353, green: 0.510, blue: 0.647, alpha: 1.000)
+
+    static let GrayColor =  UIColor(red:0.373, green: 0.373, blue: 0.376, alpha: 1.000)
+
+    static let PanelColor =  UIColor(red:0.212, green: 0.227, blue: 0.267, alpha: 1.000)
+
+    static let SecondaryColor =  UIColor(red:0.141, green: 0.220, blue: 0.282, alpha: 1.000)
+
+    static let PrimaryColor =  UIColor(red:0.129, green: 0.133, blue: 0.145, alpha: 1.000)
+}
